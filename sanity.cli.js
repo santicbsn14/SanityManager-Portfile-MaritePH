67553860e4599c10915d40e8b45f54d28f570c00
santiago_viale <santicbsn9@gmail.com>
2024-04-03 12:01:29 -0300

    feat: bootstrap sanity studio

---
+++ sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'yb8z4v1v',
+    dataset: 'production'
+  }
+})
---
+++ sanity.config.js
@@ -0,0 +1,23 @@
+import {defineConfig} from 'sanity'
+import {structureTool} from 'sanity/structure'
+import {visionTool} from '@sanity/vision'
+//import {googleMapsInput} from '@sanity/google-maps-input'
+import {schemaTypes} from './schemaTypes'
+
+export default defineConfig({
+  name: 'default',
+  title: 'WebSite-MariaTeresaGutierrezPH',
+
+  projectId: 'yb8z4v1v',
+  dataset: 'production',
+
+  plugins: [
+    structureTool(),
+    visionTool(),
+    //googleMapsInput(),
+  ],
+
+  schema: {
+    types: schemaTypes,
+  },
+})
